0c0b9bfeb1ec176785b04048b7b61431c21fdc58
AymanElsawy <aymanmoka94@icloud.com>
2023-12-11 17:00:32 +0200

    login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,63 @@
+import { CookieService } from 'ngx-cookie-service';
+import { AuthService } from './../../services/auth.service';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm!: FormGroup;
+  emailErrorMessage!: string;
+  passwordErrorMessage!: string;
+
+  private AuthService = inject(AuthService); // inject the AuthService
+  private ToastrService = inject(ToastrService); // inject the ToastrService
+  private CookieService = inject(CookieService); // inject the CookieService
+  ngOnInit() {
+    this.formInit(); // initialize the form
+
+
+  }
+
+  formInit() {
+    this.loginForm = new FormGroup({
+      'email': new FormControl('', Validators.required),
+      'password': new FormControl('', Validators.required)
+    });
+  }
+
+  login() {
+    this.emailErrorMessage = ''; // reset the error message
+    this.passwordErrorMessage = ''; // reset the error message
+    const email = this.loginForm.value.email; // get the email and password
+    const password = this.loginForm.value.password;
+    this.AuthService.login(email, password).subscribe({
+      next: (res) => {
+        this.CookieService.set('token', res.token); // set the token in the cookie
+        this.loginForm.reset(); // reset the form
+        this.ToastrService.success('Login Successful', 'Success', {
+          positionClass: 'toast-top-center'
+        }); // show success
+      },
+      error: (err) => {
+        this.ToastrService.error(err.error.message, 'Error', {
+          positionClass: 'toast-top-center'
+        }); // show error
+        if (err.status == 404) this.emailErrorMessage = err.error.message; // set the error message
+        if (err.status == 401) this.passwordErrorMessage = err.error.message; // set the error message
+      },
+      complete: () => {
+        this.emailErrorMessage = ''; // reset the error message
+        this.passwordErrorMessage = ''; // reset the error message
+      }
+    })
+  }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { } // inject http client
+
+  login(email: string, password: string): Observable<{ message: string, token: string }> { // login function
+    return this.http.post<{ message: string, token: string }>('http://localhost:5000/api/login', { // send post request
+      email, password // send email and password
+    });
+  }
+}
